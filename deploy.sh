48dd98a918419c56553903435ce9bc508dc83b57
mik <mail@kissel.ch>
2022-10-26 22:52:48 +0200

    fix scp for images upload

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ if [ -z "$1" ]
 then
   echo "Plase enter \$1 for username and \$2 for domain"
   else
-    scp web/* $1@$2:www/kissel.ch/ict/
+    scp -r web/* images $1@$2:www/kissel.ch/ict/
 fi
 exit
