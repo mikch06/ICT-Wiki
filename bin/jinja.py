250151315667233dd31100b384d12488e373ab4f
mik <mail@kissel.ch>
2023-01-28 17:54:59 +0100

    work

--- bin/jinja.py
+++ bin/jinja.py
@@ -1,9 +1,14 @@
 # write_messages.py
+from pathlib import Path
+
+
 
 from jinja2 import Environment, FileSystemLoader
 from datetime import datetime
 
-
+# Filepath
+source = 'pages'
+dest = 'web'
 
 environment = Environment(loader=FileSystemLoader("./templates/"))
 template = environment.get_template("results.html")
@@ -12,12 +17,24 @@ template = environment.get_template("results.html")
 
 
 
-for student in students:
+# for student in students:
+#
+#     results_filename = "./web/students_results.html"
+#     results_template = environment.get_template("results.html")
+#
+#
+#     with open(results_filename, mode="w", encoding="utf-8") as results:
+#         results.write(results_template.render(context))
+#         print(f"... wrote {results_filename}")
+
+# Generate html files from markdown files
+mdowns = Path(source).glob('*')
+for file in mdowns:
 
     results_filename = "./web/students_results.html"
     results_template = environment.get_template("results.html")
 
-
     with open(results_filename, mode="w", encoding="utf-8") as results:
-        results.write(results_template.render(context))
-        print(f"... wrote {results_filename}")
+        #results.write
+        results.write(results_template.name)
+        print(f"... wrote {file}")
