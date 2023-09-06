93ddb09e9ffd9add8853c7ee0680d21976b53ba1
mik <mail@kissel.ch>
2023-09-06 22:25:29 +0200

    update script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,8 +2,8 @@
 # deploy generated html files to web
 set -x
 
-prod_dir='../www/kissel.ch/wein'
-test_dir='web'
+prod_dir='/home/mikcyons/www/kissel.ch/ict/'
+test_dir='/var/www/ict/'
 
 test() {
   if [ ! -d $test_dir ];
@@ -12,8 +12,8 @@ test() {
   fi
 
   echo "Deployment to test."
-  rm -rf $test_dir/posts
-  cp -r output/* $test_dir
+  rm -rf $test_dir/*
+  cp -r web/* $test_dir
 }
 
 prod() {
@@ -23,9 +23,9 @@ prod() {
   fi
 
   echo "PRODUCTION Deployment."
-  rm -rf prod_dir/posts
-  cp -r output/* prod_dir
-}}
+  rm -rf prod_dir/*
+  cp -r web/* prod_dir
+}
 
 case "$1" in
         test)
@@ -34,7 +34,6 @@ case "$1" in
         prod)
                 prod
                 ;;
-
         *)
                 echo "Please use option $0 {test|prod}"
 esac
