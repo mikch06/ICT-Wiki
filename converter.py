d01db32c2b9e5021f0f478c9c3c112c832ee1f29
mik <mail@kissel.ch>
2023-11-18 00:19:34 +0100

    edit pagefind code

--- converter.py
+++ converter.py
@@ -99,13 +99,12 @@ path = sorted(os.listdir(dest))
 
 homepage = """
 <h1>ICT wiki</h1>
-<link href="_pagefind/pagefind-ui.css" rel="stylesheet">
-<script src="_pagefind/pagefind-ui.js" type="text/javascript"></script>
-
+<link href="pagefind/pagefind-ui.css" rel="stylesheet">
+<script src="pagefind/pagefind-ui.js"></script>
 <div id="search"></div>
 <script>
     window.addEventListener('DOMContentLoaded', (event) => {
-        new PagefindUI({ element: "#search" });
+        new PagefindUI({ element: "#search", showSubResults: true });
     });
 </script>
 """
